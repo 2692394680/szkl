e75a6411b2511cad5e9feb911b5ad4dc5d249005
smallnew <luo2692394680@163.com>
2022-07-11 09:37:38 +0800

    chore(eslint): 新增eslint相关配置

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<script setup lang="ts">
+</script>
+
+<template>
+123
+</template>
+
+<style scoped lang="less">
+
+</style>
