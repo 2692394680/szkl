2befa5438eb4b94112c9b73ec6a2b0d133a0a405
SmallNew <luo2692394680@163.com>
2022-08-03 16:51:15 +0800

    网站地图新增备案信息

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,6 +3,7 @@ import { prefix } from '@/config/global'
 import LayoutHeader from './componenets/LayoutHeader.vue'
 import LayoutSidebar from './componenets/LayoutSidebar.vue'
 import LayoutContent from './componenets/LayoutContent.vue'
+import LayoutFooter from './componenets/LayoutFooter.vue'
 import React from 'react'
 
 const name = `${prefix}-base-layout`
@@ -18,23 +19,31 @@ export default defineComponent({
     const renderContent = () => {
       return (<LayoutContent/>)
     }
+    const renderFooter = () => {
+      return (<LayoutFooter/>)
+    }
     return {
       renderHeader,
       renderSidebar,
-      renderContent
+      renderContent,
+      renderFooter
     }
   },
   render() {
     const header = this.renderHeader()
     const sidebar = this.renderSidebar()
     const content = this.renderContent()
+    const footer = this.renderFooter()
     return (
       <div>
         <t-layout>
           <t-header>{header}</t-header>
           <t-layout>
             <t-aside>{sidebar}</t-aside>
-            <t-content>{content}</t-content>
+            <t-layout>
+              <t-content>{content}</t-content>
+              <t-footer>{footer}</t-footer>
+            </t-layout>
           </t-layout>
         </t-layout>
       </div>
