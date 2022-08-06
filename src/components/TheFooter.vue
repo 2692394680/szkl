97ee99f4ea829299e3b488966457b220534b23ac
codeWYX <2429921892@qq.com>
2022-08-06 12:57:06 +0800

    2022年8月6日12:56:54 新增备案

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts"></script>
 <template>
 <div class="flex justify-center items-center mb-4">
-    <img class="mr-2" src="@/assets/备案图标.png">
-    <a class="banquan" style="color:#000;" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=湘ICP备2022015195号"
-        target="_blank">湘ICP备2022015195号</a>
+  <div style="position:fixed; left:0px; bottom:0px; width:100%; ">
+
+    <div style="display:flex;justify-content:center;align-items:center;background-color:black;height: 50px;">
+      <img src="https://szkelian.oss-cn-hangzhou.aliyuncs.com/2429921892/%E5%A4%87%E6%A1%88/beian.png">
+      <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=43052402000208"
+         style="display:inline-block;text-decoration:none;height:20px;line-height:20px;" rel="noopener">
+        <img src="" style="float:left;">
+        <b style="color:white; margin-left: 10px;"> 湘公网安备43052402000208号</b>
+      </a>
+      <span style="margin:0 10px">|</span>
+      <a target="_blank" href="http://beian.miit.gov.cn/"
+         style="display:inline-block;text-decoration:none;height:20px;line-height:20px;" rel="noopener">
+        <b style="color:white; margin-left: 10px;"> 湘ICP备2022015195号</b>
+      </a>
+    </div>
+  </div>
 </div>
 </template>
 <style lang="less"></style>
