c0519e0c6901022744c79f55119d894f20375733
smallnew <luo2692394680@163.com>
2022-10-03 14:25:36 +0800

    feat: 表格ID新增点击复制

--- src/store/index_store.ts
+++ src/store/index_store.ts
@@ -1,6 +1,36 @@
-import { createPinia } from 'pinia'
+import { createPinia, defineStore } from 'pinia'
+import Clipboard from 'clipboard'
+import { MessagePlugin } from 'tdesign-vue-next'
 
 const store = createPinia()
 export { store }
 
+export const useIndexStore = defineStore('indexStore', {
+  state: () => ({
+
+  }),
+  actions: {
+    async copyHandle(code) {
+      const clipboard = new Clipboard('.copy', {
+        text: function() {
+          return code
+        }
+      })
+      await clipboard.on('success', (e) => {
+        console.log('复制成功', e)
+        MessagePlugin.success('复制成功')
+        // 释放内存
+        clipboard.destroy()
+      })
+      await clipboard.on('error', (e) => {
+        // 不支持复制
+        console.log('该浏览器不支持自动复制', e)
+        MessagePlugin.error('该浏览器不支持自动复制')
+        // 释放内存
+        clipboard.destroy()
+      })
+    }
+  }
+})
+export const getIndexStore = () => useIndexStore(store)
 export default store
