5ef20b31beb4c13cf5adc7441884a2e92a7ea8de
smallnew <luo2692394680@163.com>
2022-10-06 14:27:36 +0800

    refactor: 路由名称修改

--- src/router/modules/common_router.ts
+++ src/router/modules/common_router.ts
@@ -34,17 +34,17 @@ export default [{
       path: 'network',
       name: 'Network',
       component: () => import('@/views/device/NetworkView.vue'),
-      meta: { title: '联网设备' }
+      meta: { title: '拥有权设备' }
     }, {
       path: 'auth',
       name: 'Auth',
       component: () => import('@/views/device/AuthView.vue'),
-      meta: { title: '授权设备' }
+      meta: { title: '使用权设备' }
     }, {
       path: 'record',
       name: 'Record',
       component: () => import('@/views/device/RecordView.vue'),
-      meta: { title: '授权记录' }
+      meta: { title: '设备授权' }
     }
   ]
 }, {
